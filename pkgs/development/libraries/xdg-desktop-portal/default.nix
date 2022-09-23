smarwei/nixpkgs

commit c3b07a052fadf19bfa6b7a26e1c12125da4ef71d

xdg-desktop-portal: 1.12.1 → 1.14.3
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.14.0
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.14.1
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.14.2
https://github.com/flatpak/xdg-desktop-portal/releases/tag/1.14.3

icon validator re-introduced:
https://github.com/flatpak/xdg-desktop-portal/commit/0eaacbada1b7da1ae4a82ff440aaf7d58e532dd3

pkgs/development/libraries/xdg-desktop-portal/default.nix
@@ -5,7 +5,8 @@
 , fetchFromGitHub
 , fetchpatch
 , flatpak
-, fuse
+, fuse3
+, systemdMinimal
 , geoclue2
 , glib
 , gsettings-desktop-schemas
@@ -14,6 +15,8 @@
 , libxml2
 , nixosTests
 , pipewire
+, gdk-pixbuf
+, librsvg
 , python3
 , pkg-config
 , stdenv
@@ -24,7 +27,7 @@
 
 stdenv.mkDerivation rec {
   pname = "xdg-desktop-portal";
-  version = "1.12.1";
+  version = "1.14.3";
 
   outputs = [ "out" "installedTests" ];
 
@@ -32,16 +35,8 @@ stdenv.mkDerivation rec {
     owner = "flatpak";
     repo = pname;
     rev = version;
-    sha256 = "1fc3LXN6wp/zQw4HQ0Q99HUvBhynHrQi2p3s/08izuE=";
+    sha256 = "CDqTQQ8dOatOxDKR+HiVQgSJKxH6ZstskKdR84U/870=";
   };
-
-  patches = [
-    # Hardcode paths used by x-d-p itself.
-    (substituteAll {
-      src = ./fix-paths.patch;
-      inherit flatpak;
-    })
-  ];
 
   nativeBuildInputs = [
     autoreconfHook
@@ -54,12 +49,17 @@ stdenv.mkDerivation rec {
     acl
     dbus
     flatpak
-    fuse
+    fuse3
+    systemdMinimal # libsystemd
     glib
     gsettings-desktop-schemas
     json-glib
     libportal
     pipewire
+
+    # For icon validator
+    gdk-pixbuf
+    librsvg
 
     # For document-fuse installed test.
     (python3.withPackages (pp: with pp; [

pkgs/development/libraries/xdg-desktop-portal/fix-paths.patch
@@ -1,13 +1,0 @@
-diff --git a/src/notification.c b/src/notification.c
-index 5412609..4243e98 100644
---- a/src/notification.c
-+++ b/src/notification.c
-@@ -366,7 +366,7 @@
-   int status;
-   g_autofree char *err = NULL;
-   g_autoptr(GError) error = NULL;
--  const char *icon_validator = LIBEXECDIR "/flatpak-validate-icon";
-+  const char *icon_validator = "@flatpak@/libexec/flatpak-validate-icon";
-   const char *args[6];
- 
-   if (G_IS_THEMED_ICON (icon))

commit 80139f0950c204017f3f51e4bbf853ea720f3e22

xdg-desktop-portal: fix icon validation
Validation requires bubblewrap, but the current build system does not enforce it being present.

Additionally, the validation itself needs to have thumbnailer Nix store paths available in sandbox. Let’s add a patch from Flatpak and introduce a test verifying it still works.

This was probably broken since https://github.com/NixOS/nixpkgs/commit/c3b07a052fadf19bfa6b7a26e1c12125da4ef71d

pkgs/development/libraries/xdg-desktop-portal/default.nix
@@ -3,9 +3,9 @@
 , autoreconfHook
 , dbus
 , fetchFromGitHub
-, fetchpatch
 , flatpak
 , fuse3
+, bubblewrap
 , systemdMinimal
 , geoclue2
 , glib
@@ -20,12 +20,12 @@
 , python3
 , pkg-config
 , stdenv
-, substituteAll
+, runCommand
 , wrapGAppsHook
 , enableGeoLocation ? true
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "xdg-desktop-portal";
   version = "1.15.0";
 
@@ -33,10 +33,20 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "flatpak";
-    repo = pname;
-    rev = version;
+    repo = "xdg-desktop-portal";
+    rev = finalAttrs.version;
     sha256 = "sha256-Kw3zJeGwPfw1fDo8HsgYmrpgCk/PUvWZPRloKJNAJVc=";
   };
+
+  patches = [
+    # The icon validator copied from Flatpak needs to access the gdk-pixbuf loaders
+    # in the Nix store and cannot bind FHS paths since those are not available on NixOS.
+    (runCommand "icon-validator.patch" { } ''
+      # Flatpak uses a different path
+      substitute "${flatpak.icon-validator-patch}" "$out" \
+        --replace "/icon-validator/validate-icon.c" "/src/validate-icon.c"
+    '')
+  ];
 
   nativeBuildInputs = [
     autoreconfHook
@@ -50,6 +60,7 @@ stdenv.mkDerivation rec {
     dbus
     flatpak
     fuse3
+    bubblewrap
     systemdMinimal # libsystemd
     glib
     gsettings-desktop-schemas
@@ -83,6 +94,11 @@ stdenv.mkDerivation rec {
   passthru = {
     tests = {
       installedTests = nixosTests.installed-tests.xdg-desktop-portal;
+
+      validate-icon = runCommand "test-icon-validation" { } ''
+        ${finalAttrs.finalPackage}/libexec/xdg-desktop-portal-validate-icon --sandbox 512 512 ${../../../applications/audio/zynaddsubfx/ZynLogo.svg} > "$out"
+        grep format=svg "$out"
+      '';
     };
   };
 
@@ -92,4 +108,4 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ jtojnar ];
     platforms = platforms.linux;
   };
-}
+})